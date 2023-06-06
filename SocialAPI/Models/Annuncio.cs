bbb880629c63fbdaf98761760db163902f843a68
cristiancazzaniga <cristian.cazzaniga@issgreppi.it>
2023-06-07 00:27:16 +0200

    fix

--- SocialAPI/Models/Annuncio.cs
+++ SocialAPI/Models/Annuncio.cs
@@ -8,7 +8,6 @@ namespace SocialAPI.Models
         public string Titolo { get; set; } = null!;
         public string Descrizione { get; set; } = null!;
         public string Media { get; set; } = null!;
-        public DateTime DataPubblicazione { get; set; }
 
     }
 }
